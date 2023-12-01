07f044e35eaf7abcbfa730d356a1a90bde60476a
JaquelineTorres <jaqueline.torres.perez.unam@gmail.com>
2023-12-01 13:53:57 -0600

    Add the OOP.java, Student.java files

---
+++ Exercises/OPP/src/Logic/OPP.java
@@ -0,0 +1,7 @@
+package Logic;
+
+public class OPP {
+    public static void main(String []args){
+
+    }
+}
---
+++ Exercises/OPP/src/Logic/Student.java
@@ -0,0 +1,9 @@
+package Logic;
+
+public class Student {
+    //Atributes
+    int id;
+    String name;
+    String lastName;
+
+}
